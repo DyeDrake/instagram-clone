827a58797d69d3ae18f933901690ec2e4d7e7e88
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-03-31 17:50:55 +0000

    Initialize the Menu component and Update the Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
-import "../styles/navigation.css"
+import "../styles/navigation.css";
+import logo from  "../images/logo.png";
+import searchIcon from "../images/searchIcon.png";
 
 function Navigation(){
     return(
         <div className="navigation">
             <div className="container">
-                <img className="logo" src="" alt="Instagram Logo"/>
+                <img className="logo" src={logo} alt="Instagram Logo"/>
                 <div className="search">
-                    <img src="" alt="Search Icon" className="search-icon"/>
+                    <img src={searchIcon} alt="Search Icon" className="search-icon"/>
                     <span className="search-text">Search</span>
                 </div>
             </div>
