b85cadbdab2a5d13dd13fd297f1ac629e1852cdb
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-04-02 03:12:40 +0000

    Initialize Main body of the Clone

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,23 @@
 import "../styles/sidebar.scss";
 import sticky from "react-sticky-el";
 import Profile from "./Profile";
-import Suggestions from "./Suggestions"
+import Suggestions from "./Suggestions";
+import Footer from "./Footer";
+import image from "../images/profileImage.jpg";
 function Sidebar(){
     return(
-        <div className="sidebar">
-
-        </div>
+            <sticky topOffset={-80}>
+                <div className="sidebar">
+                    <Profile
+                        username="wendy m.A D."
+                        caption="Wendy M. A. Darling"
+                        urlText="switch"
+                        iconSize="large"
+                        image={image}/>
+                </div>
+                <Suggestions/>
+                <Footer/>
+            </sticky>
     )
 }
 
