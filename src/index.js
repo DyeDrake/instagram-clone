15224e8d720f617d7320cfe8acf30db18072dce4
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-04-02 22:21:57 +0000

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import SignUpPage from './SignUpPage';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
