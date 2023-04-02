c917585aa2eecd070fedadf712fd28568689e915
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-04-02 17:04:35 +0000

    sign up complete, remaining the footer and the horizontal line on the 'OR'

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = () => {
      <div className="box-1-logo">
     <img src={instagramLogoImage} alt="#" className="instagram-logo"/>
   </div>
-    <div className="signUp-message-1">
+    <div className="sign-up-message-1">
       Sign up to see photos and videos from your friends.
     </div>
      <button className="log-in-with-facebook"> <img className="Facebook-icon" src={facebookImage} alt=""/>
@@ -34,27 +34,34 @@ const SignUp = () => {
     <div className="input-box-password">
       <input type="text" placeholder="Password"/>
     </div>
-    <div className="secondary-message-1">
+    <div className="secondary-message" id="m1">
       People who use our service may have uploaded your contact information to Instagram.
-      <a href=""> Learn more</a>
+      <a className="links" href=""> Learn more</a>
     </div>
-      <div className="secondary-message-2">
+    <br/>
+      <div className="secondary-message" id="m2">
         By signing up, you agree to our <a href="">Terms</a>,
-          <a href="">Privacy Policy</a> and
-            <a href=""> Cookie Policy</a>
+          <a className="links" href=""> Privacy Policy</a> and
+            <a className="links" href=""> Cookie Policy</a>
       </div>
+    <div>
+       <button>
+        Sign up
+      </button>
+    </div>
   </div>
   <div className="box-2">
-      <p>Have an account? <span className="log-in"></span>Sign in</p>
+      <p>Have an account? <span className="log-in"></span> <a className="links" href="">Sign in</a> </p>
   </div>
   <div className="get-the-app">
-    <p>Get the app.</p>
+    <p>Get the app</p>
   </div>
   <div className="google-play-microsoft-store-box">
     <img src={googleImage} alt="#" className="google-play-store"/>
     <img src={microsoftImage} alt="#" className="microsoft-store"/>
   </div>
 </div>
+      // <footer className></footer>
     );
 };
 
