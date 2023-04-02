b1a101e1326765aa77018c3f2bbd39c5e086685e
Dye Drake <102999438+DyeDrake@users.noreply.github.com>
2023-04-02 21:54:19 +0000

    refactored a file

---
+++ src/SignUpPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/signUpPage.css';
+import SignUpPage from "./components/SignUpPage";
+
+function SignUpPage(){
+    return(
+<div className="App">
+    <SignUpPage />
+</div>
+    );
+}
+
+export default SignUpPage;
